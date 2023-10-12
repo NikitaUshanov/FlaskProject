4aacfd062ae641f7ba57d79174ae9505186a7aa1
NikitaUshanov <nikita.ushanov.y@gmail.com>
2023-10-12 21:29:26 +0300

    add redirect

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,23 @@
 from flask import Flask
+from flask_login import LoginManager
 
 from config import Config
 from app.extensions import db
+from app.models.user import User
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    login_manager = LoginManager()
     app.config.from_object(config_class)
 
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.get(user_id)
+
     db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = "login.login"
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
--- app/login/routes.py
+++ app/login/routes.py
@@ -1,15 +1,5 @@
-from flask import render_template, redirect, request
-from flask_login import user_unauthorized
+from flask import render_template
 from app.login import bp
-
-
-login_url_exceptions = ["/login", "/register"]
-
-
-@bp.before_request
-def unauthorized():
-    if user_unauthorized and request.url_rule.rule not in login_url_exceptions:
-        redirect("/login")
 
 
 @bp.route("/login")
